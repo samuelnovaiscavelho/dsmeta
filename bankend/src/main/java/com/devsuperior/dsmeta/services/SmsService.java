1685a7fd4fa03309d940383d86b3d01eacea70d6
samuelnovaiscavelho <samuel.novais247@gmail.com>
2022-11-20 23:29:22 -0300

    Axios, useEffect first request

--- bankend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ bankend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dsmeta.services;
 
+import java.text.DecimalFormat;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -34,8 +36,7 @@ public class SmsService {
 		
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 		
-		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
-				+ " com um total de R$ " + String.format("%.2f", sale.getAmount());
+		String msg = String.format("O vendedor %s foi destaque em %s com um total de R$ %.2f", sale.getSellerName(), date, sale.getAmount());
 
 		Twilio.init(twilioSid, twilioKey);
 
